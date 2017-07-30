d7ac3f8f74ca7a0d9c80c8ddd827d695471183c1
Jose Pablo Ramirez <jp.ramirezpra@gmail.com>
2017-07-30 03:56:10 -0500

    Updates to UI and Functionality

--- Refuge/public/javascripts/refugeHereMap.js
+++ Refuge/public/javascripts/refugeHereMap.js
@@ -1,4 +1,4 @@
-﻿(function () {
+﻿window.funcs = (function () {
     'use strict';
 
     //Step 1: initialize communication with the platform
@@ -10,10 +10,15 @@
 
     var platform = new H.service.Platform({
         'app_id': 'siqtyDjqWHW6QTGXwbtY',
-        'app_code': '9HTj_sF6NO0dometkPDDfg'
+        'app_code': '9HTj_sF6NO0dometkPDDfg',
+        useCIT: true,
     });
 
+    platform.setUseHTTPS(true);
+
     var map, behavior, ui;
+
+    var campData;
 
     function onGeoClick(id) {
         alert("you clicked a GEO");
@@ -34,18 +39,76 @@
         behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
         ui = H.ui.UI.createDefault(map, defaultLayers);
+
+
+
     }
 
     function load() {
-        $.get("https://cle.cit.api.here.com/2/search/all.json?&app_code=" + app_code + "&app_id=" + app_id +"&layer_id="+1, function (data) {
-            console.dir(data);
-            //plot geometries
+        //$.get("https://cle.cit.api.here.com/2/search/all.json?&app_code=" + app_code + "&app_id=" + app_id +"&layer_id="+"TESTLAYER", function (data) {
+        //    debugger;
+        //    console.dir(data);
+        //    //plot geometries
+        //    for (var i = 0; i < data.geometries.length; i++) {
+        //        console.log(data.geometries[i]);
+        //        map.addObject(new H.map.Polygon(new H.geo.Strip(data.geometries[i].geometry)));
+        //    }
+            
+        //});
 
+        // Create an icon, an object holding the latitude and longitude, and a marker:
+        var icon = new H.map.Icon('images/camp.png')
 
-            alert("Load was performed.");
+        $.get("/plotpoints", function (data) {
+            //PLot Camps
+            var camps = JSON.parse(data);
+            delete camps._id;
+            delete camps._rev;
+            campData = camps;
+
+            var coords;
+            var marker;
+            var i = 0;
+            var id;
+            var correction = $('.ui.stackable.menu').height() + 10;
+            while (camps[i] != undefined) {
+                coords = { lat: camps[i].LAT_DD, lng: camps[i].LONG_DD };
+                marker = new H.map.Marker(coords, { icon: icon });
+                marker.data = camps[i].FID;
+                marker.addEventListener("tap", function (evt) {
+                    $('.campList ul').children().removeClass('active');
+                    $('.campList ul').scrollTop(0);
+                    $('.campList ul').animate({ scrollTop: $('#marker' + this.data).offset().top - correction }, 1000);
+                    $('#marker' + this.data).addClass('active');
+                    updateMap(this.data);
+                });
+
+                map.addObject(marker);
+                i++
+            }
         });
+    }
+
+    function updateMap(id) {
+        var i = 0;
+        while (campData[i] != undefined) {
+            if (campData[i].FID == id) {
+                map.setCenter({ lat: campData[i].LAT_DD, lng: campData[i].LONG_DD });
+                map.setZoom(10);
+            }
+            i++;
+        }
     }
 
     init();
     load();
+
+    return {
+        centerMap: function (lat, lng, id) {
+            $('.campList ul').children().removeClass('active');
+            $('#marker' + id).addClass('active');
+            map.setCenter({ lat: lat, lng: lng });
+            map.setZoom(10);
+        }
+    }
 }());
--- Refuge/routes/index.js
+++ Refuge/routes/index.js
@@ -2,64 +2,67 @@
 var express = require('express');
 var request = require('request');
 var fs = require('fs');
+var nano = require('nano')('http://localhost:5984');
 var router = express.Router();
 
 var hereUrl = "https://cle.cit.api.here.com";
+//var hereUrl = "http://cle.api.here.com/";
+
+
 
 /* GET home page. */
 router.get('/', function (req, res) {
+    var refuge = nano.db.use('refugedb');
 
-    res.render('index', { title: 'REFUGE', array: getListofRefugee() });
+    refuge.get('refugeecamps', { NAME: { "$not": "" } }, function (err, body) {
+        delete body._id;
+        delete body._rev;
+        res.render('index', { title: 'REFUGE', camps: body });
+    });
 });
 
-router.get('/testDb', function (req, res) {
+router.get('/plotpoints', function (req, res) {
+    var refuge = nano.db.use('refugedb');
 
-    var sqlite3 = require('sqlite3').verbose();
-    var db = new sqlite3.Database('RefugeDatabase', sqlite3.OPEN_READWRITE, function (error) {
-        res.render('UploadFile');
-    });
+    refuge.get('refugeecamps', { NAME: { "$not": "" }, _id: false},
+        function (err, body) {
 
-    db.open();
-
-    db.run("SELECT * FROM testTable", function (err, row) {
-        if (err == null) {
-            var data = row;
-        }
-
-        res.render('UploadFile');
-    });
-
-    db.close();
+            res.send(JSON.stringify(body));
+        });
 })
 
 router.get('/uploadShapeFile', function (req, res) {
+    //var postUrl = hereUrl + '/2/layers/upload.json?app_id=siqtyDjqWHW6QTGXwbtY&app_code=9HTj_sF6NO0dometkPDDfg';
+
+    //var formData = {
+    //    file: fs.createReadStream(__dirname + '/shape/test.wkt'),
+    //    layer_id: 'TESTLAYER',
+    //};
+    //request.post({ url: postUrl, formData: formData }, function optionalCallback(err, httpResponse, body) {
+
+    //    if (err) {
+    //        return console.error('upload failed:', err);
+    //    }
+    //    console.log('Upload successful!  Server responded with:', body);
+    //});
+
+    //res.render('UploadFile');
+
     var postUrl = hereUrl + '/2/layers/upload.json?app_id=siqtyDjqWHW6QTGXwbtY&app_code=9HTj_sF6NO0dometkPDDfg';
+    var filename = __dirname + '/shape/Camps_in_Progress.zip';
 
-    var formData = {
-        file: fs.createReadStream(__dirname + '/shape/test.wkt'),
-        //app_id: 'siqtyDjqWHW6QTGXwbtY',
-        //app_code: '9HTj_sF6NO0dometkPDDfg',
-        layer_id: '1',
-        // Pass optional meta-data with an 'options' object with style: {value: DATA, options: OPTIONS} 
-        // Use case: for some types of streams, you'll need to provide "file"-related information manually. 
-        // See the `form-data` README for more information about options: https://github.com/form-data/form-data 
-        //custom_file: {
-        //    value: fs.createReadStream('/dev/urandom'),
-        //    options: {
-        //        filename: 'topsecret.jpg',
-        //        contentType: 'image/jpeg'
-        //    }
-        //}
-    };
-    request.post({ url: postUrl, formData: formData }, function optionalCallback(err, httpResponse, body) {
-
+    var req = request.post(postUrl, function (err, resp, body) {
         if (err) {
-            return console.error('upload failed:', err);
+            console.log(err);
         }
-        console.log('Upload successful!  Server responded with:', body);
-    });
+        else {
+            console.log(body);
+        }
+    })
 
-    res.render('UploadFile');
+    var form = req.form();
+    form.append('layer_id', 'TESTLAYER');
+    form.append('file', fs.createReadStream(filename));
 });
 
 function getListofRefugee() {
